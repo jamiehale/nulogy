495787ecac8e6ceb055c729c53362f34809cba7c
Jamie Hale <jamie@smallarmyofnerds.com>
2016-11-02 14:32:21 -0400

    Refactor specs to make it clear that each one is doing the same type of calculation

--- spec/nulogy/markup_calculator_spec.rb
+++ spec/nulogy/markup_calculator_spec.rb
@@ -12,24 +12,41 @@ module Nulogy
 
       let( :calculator ) { MarkupCalculator.new }
 
-      it 'says free stuff is free' do
-        expect( calculator.calculate( 0.00, 0 ) ).to eq( 0.00 )
-      end
-
-      it 'says non-free stuff gets marked up by the flat rate' do
-        expect( calculator.calculate( 1.00, 0 ) ).to eq( 1.00 * 1.05 )
-      end
-
-      it 'says 1 person adds a 1.2% markup' do
-        expect( calculator.calculate( 1.00, 1 ) ).to eq( 1.00 * 1.05 * 1.012 )
-      end
-
-      it 'says 2 people adds a 2.4% markup' do
-        expect( calculator.calculate( 1.00, 2 ) ).to eq( 1.00 * 1.05 * 1.024 )
-      end
-
-      it 'says 100 people adds a 120% markup' do
-        expect( calculator.calculate( 1.00, 100 ) ).to eq( 1.00 * 1.05 * 2.20 )
+      [
+        {
+          description: 'free stuff is free',
+          base: 0.00,
+          people: 0,
+          output: 0.00
+        },
+        {
+          description: 'non-free stuff gets marked up by the flat rate of 5%',
+          base: 1.00,
+          people: 0,
+          output: 1.00 * 1.05
+        },
+        {
+          description: '1 person adds a 1.2% markup',
+          base: 1.00,
+          people: 1,
+          output: 1.00 * 1.05 * 1.012
+        },
+        {
+          description: '2 people add a 2.4% markup',
+          base: 1.00,
+          people: 2,
+          output: 1.00 * 1.05 * 1.024
+        },
+        {
+          description: '100 people adds a 120% markup',
+          base: 1.00,
+          people: 100,
+          output: 1.00 * 1.05 * 2.20
+        }
+      ].each do |example|
+        it "says #{example[:description]}" do
+          expect( calculator.calculate( example[ :base ], example[ :people ] ) ).to eq( example[ :output ] )
+        end
       end
 
     end
