73a17786777461d3a8109e5e5ab899c6aa172dfd
Jamie Hale <jamie@smallarmyofnerds.com>
2016-11-02 14:34:31 -0400

    Extract constants in calculator

--- lib/nulogy/markup_calculator.rb
+++ lib/nulogy/markup_calculator.rb
@@ -3,9 +3,12 @@ module Nulogy
   # Calculates the markup on a price based on a collection of markups and a
   # set of markup identifiers.
   class MarkupCalculator
-    
+   
+    FLAT_RATE = 0.05
+    PEOPLE_RATE = 0.012
+
     def calculate( base_price, people )
-      base_price * 1.05 * ( 1.00 + people * 0.012 )
+      base_price * ( 1.00 + FLAT_RATE ) * ( 1.00 + people * PEOPLE_RATE )
     end
 
   end
